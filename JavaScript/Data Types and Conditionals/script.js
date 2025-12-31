425e342f6fff86396f44c740bcd30ed95a87e8da
Aaruush <aaruushbhowmick@gmail.com>
2025-12-31 14:09:31 +0000

    Added the function

--- JavaScript/Data Types and Conditionals/script.js
+++ JavaScript/Data Types and Conditionals/script.js
@@ -3,7 +3,7 @@ let n = 233;
 console.log(n);
 
 // String Data Types
-let username = "Aaryan";
+let food = "Pizza";
 console.log(name);
 
 // Boolean Data Types
@@ -20,3 +20,10 @@ console.log(user.age);
 // Array Data Types
 let arr = [3, 9, 6, 8];
 console.log(arr);
+
+// Function Data Types
+let greet = (name) => {
+    console.log(`Hello ${name}!`);
+}
+
+console.log(greet);
